727f5f14bc8e06c58df3d13af80987f8a62e45d2
月落 <gaozijian0411@foxmail.com>
2020-09-13 20:50:30 +0800

    启发更新

--- src/matrix_tool.ts
+++ src/matrix_tool.ts
@@ -1,16 +1,22 @@
 import * as tf from "@tensorflow/tfjs"
 //如果等于则为1 否则则为0
+//相等比较
 export function equalMap<T extends tf.Tensor>(ts: T, equto: number) {
     return ts.div(equto).sub(1).abs().lessEqual(0);
 }
-//1-0 变换
+//此处应有大于比较  由此可得 所有比较判断
+
+//1-0 变换 即not运算
 function reverseBool(ts: tf.Tensor) {
     return ts.sub(1).abs();
 }
-//0-1 变为  -1 1
+//0-1 变为  -1 1 符号化运算
 function symlize(ts: tf.Tensor) {
     return ts.mul(2).sub(1);
 }
+//此处应有与或非 异或 
+
+
 export function expandTo4D(ts: tf.Tensor2D): tf.Tensor4D {
     //这个把2d featuremap变为4d可以直接进行卷积操作的featuremap或kernel
     //也就是直接对featuremap进行卷积
